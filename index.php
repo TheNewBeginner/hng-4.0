6a34fa6156d51130455fc86470da5e312981fc77
TheNewBeginner <davidyega69@gmail.com>
2018-04-13 09:47:34 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+	<!-- google fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet">
+
+	<!-- bootraph CDN -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- local stylesheet -->
+	<link rel="stylesheet" href="css/index.css">
+
+</head>
+<body>
+	<section>
+		<div class="overall_header">
+			<div class="container">
+				<div class="row">
+					<div class=" center black_holder">
+						<p class="motto">a place where peace exists</p>
+						<p class="name">starview palace</p>
+						<div class="time-box">
+							<div class="timeFunction">
+								<p class="time">
+									<?php echo date("h:i:s A"); ?>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</body>
+</html>
